0083359206e77722bfc4851c9507f0a9eee94900
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-02-21 12:21:25 -0500

    ListView

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  //conss HomePageTemp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body:ListView(
+      children:<Widget>[
+        ListTile(
+          title:Text('ListTile Title'),
+      ),
+      Divider(),
+       ListTile(
+          title:Text('ListTile Title'),
+      ),
+      Divider(),
+       ListTile(
+         
+          title:Text('ListTile Title'),
+      ),
+      ]
+      ),
+    );
+  }
+}
