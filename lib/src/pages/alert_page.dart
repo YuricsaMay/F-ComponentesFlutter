39a37722e930c0f842d3301a026d342c53523271
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-02-22 15:42:24 -0500

    widget- alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,61 @@ Widget build(BuildContext context){
     appBar: AppBar(
       title:Text('Alert Page'),
       ),
+      body:Center(
+        child:RaisedButton(
+          child:Text ('Mostrar alerta'),
+          //style:ElevatedButton.styleFrom(
+            color: Colors.blue,
+            textColor: Colors.white,
+            shape: StadiumBorder(),
+            onPressed:() => _mostrarAlert(context),
+            ),//ElevatedButton 
+           ),//Center
+        
+        
       floatingActionButton: FloatingActionButton(
         child:Icon(Icons.add_location),
         onPressed: (){
           Navigator.pop(context);
+      
         },
       ),
   );
+
 }
+void _mostrarAlert(BuildContext context){
+
+showDialog(
+  context: context,
+  barrierDismissible: true,
+  builder:(context){
+
+    return AlertDialog(
+      shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      title:Text('Titulo'),
+      content:Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+      Text('Este es el contenido de la caja de la alerta'),
+      FlutterLogo(size: 100.0)
+      ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Cancelar'),
+          onPressed: ()=> Navigator.of(context).pop(),
+        ),
+        FlatButton(
+          child: Text('Ok'),
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+);
+}
+
+  
 }
