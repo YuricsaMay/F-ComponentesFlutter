32091bfc0f2d33b8d7ea0d9470a3d01e414cc758
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-02-22 21:02:30 -0500

    datepicker

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -10,7 +10,9 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
 String _nombre='';
   String  _email='';
+String _fecha='';
 
+TextEditingController _inputFieldDateController=new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,10 +25,13 @@ String _nombre='';
           _crearInput(),
            Divider(),
           _crearEmail(),
+           Divider(),
+          _crearPassword(),
+           Divider(),
+          _crearFecha(context),
           Divider(),
           _crearPersona(),
-          Divider(),
-          _crearPassword(),
+          
         ],
       ),
     );
@@ -80,22 +85,74 @@ String _nombre='';
         borderRadius: BorderRadius.circular(20.0)
         ),
         
-        hintText:'Pasword',
-        labelText:'Pasword',
+        hintText:'Password',
+        labelText:'Password',
         suffixIcon:Icon(Icons.lock_open),
-        icon:Icon(Icons.lock_open)
+        icon:Icon(Icons.lock)
       ),
       onChanged:(valor)=>setState((){
            _email=valor;
         })
     );
        }
+        Widget _crearFecha(BuildContext context){
+    return TextField(
+   enableInteractiveSelection: false,
+   controller:_inputFieldDateController,
+      decoration:InputDecoration(
+        border:OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20.0)
+        ),
+        
+        hintText:'Fecha de nacimiento',
+        labelText:'Fecha de nacimiento',
+        suffixIcon:Icon(Icons.perm_contact_calendar),
+        icon:Icon(Icons.calendar_today)
+      ),
+      onTap:(){
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+        );
+    
+        }
+
+ Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate:new DateTime(2018),
+      lastDate: new DateTime(2025),
+    );
+  
+           /* context: context,
+            initialDate: new DateTime.now(),
+            firstDate:new DateTime(2018),
+            lastDate:new DateTime(2025),*/
+        
+            if (picked !=null){
+              setState((){
+                _fecha=picked.toString();
+                _inputFieldDateController.text=_fecha;
+              });
+            }
+        }
+
+        
+
+
+
+
+
+
+
     Widget _crearPersona(){
       return ListTile(
         title:Text('Nombre es: $_nombre'),
         subtitle: Text('Email: $_email'),
+      
       );
 
     }
       }
-  
+      
