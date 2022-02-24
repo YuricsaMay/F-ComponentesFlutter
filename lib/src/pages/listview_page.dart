03689da1e07c725176f3a19c9579dcd2e8b789de
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-02-23 20:40:15 -0500

    InfiniteScroll  e infinitescroll con futures

---
+++ lib/src/pages/listview_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatefulWidget {
+  const ListaPage({ Key? key }) : super(key: key);
+
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+  ScrollController _scroll = new ScrollController();
+  List<int> _listaNumeros=[1,2,3,4,5];
+  int _ultimoElemento = 0;
+  bool _Cargando = false;
+      @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scroll.addListener(() {
+      if (_scroll.position.pixels == _scroll.position.maxScrollExtent) {
+        //_agregar10();
+        fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scroll.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text ('Listas'),
+      ),
+  body: Stack(
+          children: [
+            _crearLista(),
+            _crearCargando(),
+          ],
+        ));
+  }
+
+  Widget _crearLista(){
+    
+
+      return RefreshIndicator(
+      onRefresh:recargar,
+      child: ListView.builder(
+          controller: _scroll,
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext contex, int index){
+
+          final imagen= _listaNumeros[index];
+
+          return FadeInImage(
+            image: NetworkImage('https://picsum.photos/500/300/?imagen=$index'),
+            placeholder:AssetImage('assets/jar-loading.gif'),
+            );
+    
+      }),
+      );
+  }
+   Future <Null> recargar() async{
+     await Future.delayed(const Duration(seconds: 2));
+      _listaNumeros.clear();
+      _ultimoElemento++;
+      _agregar10();
+  }
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoElemento++;
+      _listaNumeros.add(_ultimoElemento);
+      setState(() {});
+    }
+  }
+
+  Future fetchData() async {
+    if (_Cargando) return;
+    _Cargando = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    _agregar10();
+    _Cargando = false;
+    setState(() {});
+
+   if (_scroll.position.pixels + 100 <=
+        _scroll.position.maxScrollExtent) return;
+
+    _scroll.animateTo(_scroll.position.pixels + 100,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.fastOutSlowIn);
+
+  }
+
+  Widget _crearCargando() {
+    if (_Cargando) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[CircularProgressIndicator()],
+          ),
+          SizedBox(height: 15.0)
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,7 +6,7 @@ import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
 import 'package:componentes/src/pages/slider_page.dart';
-
+import 'package:componentes/src/pages/listview_page.dart';
 
 Map<String,WidgetBuilder>getApplicationRoutes(){ 
 return<String, WidgetBuilder>{
@@ -17,6 +17,6 @@ return<String, WidgetBuilder>{
         'animatedContainer' : (BuildContext context)=>AnimatedContainerPage(),
         'inputs' : (BuildContext context)=>InputPage(),
         'slider' : (BuildContext context) => SliderPage(),
-       
+         'list' : (BuildContext context) => ListaPage(),
 };
 }
